90e29e14a08e1bc20518e9e02fe760661a32f85f
liber1988 <92869703+liber1988@users.noreply.github.com>
2024-05-01 13:44:28 +0300

    Arrow functions

--- day-6/draft.js
+++ day-6/draft.js
@@ -1,20 +1,16 @@
-const character1 = {
-  name: "Eldrin",
-  health: 100,
-  mana: 50,
+var obj = {
+  prop1: "value1",
+  prop2: {
+    nestedProp: "nestedValue",
+  },
+  prop: [1, 2, 3, 4],
 };
 
-const character2 = {
-  name: "Mira",
-  health: 85,
-  mana: 60,
-};
+// Freeze the outer object
+Object.freeze(obj);
 
-const mergedCharacter = { ...character2, ...character1 };
-
-console.log(mergedCharacter);
-
-const copy = Object.assign({}, character1);
-copy.name = "Reuven";
-console.log(copy);
-console.log(character1);
+// Try to modify the value of nestedProp
+obj.prop2.nestedProp = "new nested value"; // This will not have an effect
+obj.prop2.check = "checking";
+obj.prop[0] = 1000;
+console.log(obj.prop); // Output: 'nestedValue'
